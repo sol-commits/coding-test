a07ae07f1e75e0a062c36ead3be94f72272fe294
sol-commits <vvluc0_2@naver.com>
2024-12-06 20:07:10 +0900

    Update 모의고사.py

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,29 +1,22 @@
-# pattern1 = [1, 2, 3, 4, 5]
-# pattern2 = [2, 1, 2, 3, 2, 4, 2, 5]
-# pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-# answers : 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열
-# return : 가장 많은 문제를 맞힌 사람이 누구인지 배열
-# 가장 높은 점수를 받은 사람이 여럿일 경우, 오름차순으로 정렬
 def solution(answers):
-    result = []
+    patterns = [
+        [1, 2, 3, 4, 5] * 2000,
+        [2, 1, 2, 3, 2, 4, 2, 5] * 1250,
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * 1000
+    ]
+
+    scores = [0] * 3;
     
-    pattern1 = [1, 2, 3, 4, 5] * 2000
-    pattern2 = [2, 1, 2, 3, 2, 4, 2, 5] * 1250
-    pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * 1000
-    count = [0] * 3;
-    
-    for answer, a, b, c in zip(answers, pattern1, pattern2, pattern3):
-        if a == answer: 
-            count[0] += 1
-        if b == answer:
-            count[1] += 1
-        if c == answer:
-            count[2] += 1
+    for idx, answer in enumerate(answers):
+        for i in range(3):
+            if patterns[i][idx] == answer:
+                scores[i] += 1
             
-    max_count = max(count)
+    max_score = max(scores)
+    highest_scores = []
     
-    for idx, value in enumerate(count):
-        if value == max_count:
-            result.append(idx + 1)
+    for idx, score in enumerate(scores):
+        if score == max_score:
+            highest_scores.append(idx + 1)
         
-    return result
+    return highest_scores
