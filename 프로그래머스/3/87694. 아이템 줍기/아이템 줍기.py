01fd9f1c68bd6ccb7d0d670ba1772db2eb9788cb
sol <vvluc0_2@naver.com>
2025-04-03 11:00:10 +0900

    [level 3] Title: 아이템 줍기, Time: 1.06 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,5 +1,4 @@
 from collections import deque
-import math
 
 def solution(rectangle, characterX, characterY, itemX, itemY):
     """
@@ -10,47 +9,37 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
     Returns:
         캐릭터가 아이템을 줍기 위해 이동해야 하는 최소 거리
     """
-    map = [[0] * 151 for _ in range(152)]
+    scale = 2
+    map_limit = 50*scale+1
+    map = [[0] * (map_limit) for _ in range(map_limit + 1)]
     moves = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-    points = {}
     
     for x1, y1, x2, y2 in rectangle:
-        for x in range(x1*3, x2*3 + 1):
-            for y in [y1*3, y2*3]:
+        for x in range(x1*scale, x2*scale + 1):
+            for y in [y1*scale, y2*scale]:
                 map[x][y] = 1
-        for y in range(y1*3, y2*3 + 1):
-            for x in [x1*3, x2*3]:
+        for y in range(y1*scale, y2*scale + 1):
+            for x in [x1*scale, x2*scale]:
                 map[x][y] = 1
-        map[x1*3][y1*3] = 1
-        map[x2*3][y1*3] = 1
-        map[x1*3][y2*3] = 1
-        map[x2*3][y2*3] = 1
-        points[(x1*3, y1*3)] = 1
-        points[(x2*3, y1*3)] = 1
-        points[(x1*3, y2*3)] = 1
-        points[(x2*3, y2*3)] = 1
     
     for x1, y1, x2, y2 in rectangle:
-        for x in range(x1*3+1, x2*3):
-            for y in range(y1*3+1, y2*3):
+        for x in range(x1*scale+1, x2*scale):
+            for y in range(y1*scale+1, y2*scale):
                 map[x][y] = 0
     
-    characterX, characterY = characterX*3, characterY*3
-    itemX, itemY = itemX*3, itemY*3
+    characterX, characterY = characterX*scale, characterY*scale
+    itemX, itemY = itemX*scale, itemY*scale
     
-    queue = deque([(characterX, characterY, 1)])
+    queue = deque([(characterX, characterY, 0)])
     map[characterX][characterY] = 0
     
     while queue:
         cur_X, cur_Y, dist = queue.popleft()
         if (cur_X, cur_Y) == (itemX, itemY):
-            return dist // 3
+            return dist // scale
         
         for move_X, move_Y in moves:
             new_X, new_Y = cur_X + move_X, cur_Y + move_Y
-            if 0 < new_X < 151 and 0 < new_Y < 151 and map[new_X][new_Y] == 1:
+            if 0 < new_X < map_limit and 0 < new_Y < map_limit and map[new_X][new_Y] == 1:
                 map[new_X][new_Y] = 0
-                # if (cur_X, cur_Y) in points:
-                #     queue.append((new_X, new_Y, dist + 2))
-                # else:
                 queue.append((new_X, new_Y, dist + 1))
