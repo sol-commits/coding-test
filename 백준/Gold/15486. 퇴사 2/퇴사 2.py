bc9435b5da3a43b0e47125b94626cacef08b5aad
sol <vvluc0_2@naver.com>
2025-05-29 12:56:54 +0900

    [Gold V] Title: 퇴사 2, Time: 2776 ms, Memory: 348740 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,6 +1,4 @@
 import sys
-from collections import defaultdict
-from heapq import heappop, heappush
 
 def solution(day_len, day_price):
     """
@@ -17,16 +15,13 @@ def solution(day_len, day_price):
     핵심 아이디어: 현재 단계의 가능한 점프 수를 이전 단계의 결과에 의존
     """  
     prices = [0] * (day_len + 1)
-    pq = []
+    max_price_by_day = 0
     for day in range(day_len):
         next_day = day + day_price[day][0] - 1
         if next_day < day_len:
-            if pq:
-                prices[next_day] = max(prices[next_day], -pq[0] + day_price[day][1])
-            else:
-                prices[next_day] = max(prices[next_day], day_price[day][1])
-        heappush(pq, (-prices[day]))
-    return -heappop(pq)
+            prices[next_day] = max(prices[next_day], max_price_by_day + day_price[day][1])
+        max_price_by_day = max(max_price_by_day, prices[day])
+    return max_price_by_day
                 
 if __name__ == "__main__":
     input = sys.stdin.readline
