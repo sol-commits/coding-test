7a57a6fe64aad2f8230d08c6fc22f5f9f8adf415
sol <vvluc0_2@naver.com>
2025-05-29 12:53:14 +0900

    [Gold V] Title: 퇴사 2, Time: 3292 ms, Memory: 411768 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,36 @@
+import sys
+from collections import defaultdict
+from heapq import heappop, heappush
+
+def solution(day_len, day_price):
+    """
+    day_price: [[time1, price1], ...]
+    - time: 상담을 완료하는데 걸리는 기간
+    - price: 상담을 했을 때 받을 수 있는 금액
+    
+    얻을 수 있는 최대 수익을 반환
+    
+    1 <= day_price_len <= 1,500,000 = 10^6
+    1 <= time <= 50, 1 <= price <= 1,000
+    
+    총 시간 복잡도: 
+    핵심 아이디어: 현재 단계의 가능한 점프 수를 이전 단계의 결과에 의존
+    """  
+    prices = [0] * (day_len + 1)
+    pq = []
+    for day in range(day_len):
+        next_day = day + day_price[day][0] - 1
+        if next_day < day_len:
+            if pq:
+                prices[next_day] = max(prices[next_day], -pq[0] + day_price[day][1])
+            else:
+                prices[next_day] = max(prices[next_day], day_price[day][1])
+        heappush(pq, (-prices[day]))
+    return -heappop(pq)
+                
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    N = int(input())
+    TP = [list(map(int, input().split())) for _ in range(N)]
+    
+    print(solution(N, TP))
