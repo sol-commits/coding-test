a65eee842af1770b8f319a0e6748a0b9b8f142c9
sol <vvluc0_2@naver.com>
2025-06-01 10:33:45 +0900

    [Gold V] Title: 동전 2, Time: 144 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,35 @@
+import sys
+
+def solution(coin_types, target, coins):
+    """
+    coins 종류의 동전을 이용해서 그 가치의 합이 target원이 되도록 하는 동전 개수의 최솟값
+    
+    1 <= coin_types <- 100, 1 <= target <= 10,000
+    1 <= coin a, b, c... <= 100,000
+    
+    22 = 15a + 5b + 4c + 3d
+    dp[0] = -1, dp[1] = -1, dp[2] = -1, dp[3] = 1, dp[4] =1,
+    dp[5] = 1, dp[6] = dp[3] + dp[3] = 2, dp[7] = dp[3] + dp[4] =2
+    dp[8] = dp[5] + dp[3] = 2, dp[9] = dp[5] + dp[4] = 2
+    dp[10] = dp[5] + dp[5], dp[11] = dp[8] + dp[3] = 3,
+    dp[12] = dp[9] + dp[3] = 2 + 1 = 3, dp[13] = dp[10] + dp[3] = 
+        
+    총 시간 복잡도: O(N)
+    핵심 아이디어: 
+    """  
+    dp = [float('inf')] * (target + 1)
+    coins.sort(reverse=True)
+    
+    for coin in coins:
+        if coin <= target:
+            dp[coin] = 1
+            for num in range(coin + 1, target + 1):
+                dp[num] = min(dp[coin] + dp[num - coin], dp[num])
+    return -1 if dp[target] == float('inf') else dp[target]
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    n, k = list(map(int, input().split()))
+    coins = list(set(int(input()) for _ in range(n)))
+
+    print(solution(n, k, coins))
